6280f989403ea08bbd23d394ddb5715513793e7b
Om Dhormare <143721889+omdhormare@users.noreply.github.com>
2024-05-26 21:09:18 +0530

    Add files via upload

---
+++ Reverse_no.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class rev
+{
+       public static void main(String[] args)
+       {
+           int d,n,sum=0;
+           Scanner s=new Scanner(System.in);
+          System.out.print("Enter Number : ");
+          n=s.nextInt();
+             System.out.print("Reverse No : ");
+          while(n>0)     
+            {
+                   d=n%10;
+                   System.out.print(d);
+                  n=n/10;
+            }
+       }
+}
